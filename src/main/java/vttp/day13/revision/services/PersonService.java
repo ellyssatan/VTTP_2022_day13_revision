8838fa22cc901ad30fe2bc85e8a54cba99084b18
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-21 10:56:34 +0800

    modified application properties

---
+++ src/main/java/vttp/day13/revision/services/PersonService.java
@@ -0,0 +1,28 @@
+package vttp.day13.revision.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vttp.day13.revision.models.Person;
+
+@Service
+public class PersonService {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public PersonService() {
+        persons.add(new Person("Mark", "Z"));
+        persons.add(new Person("Elon", "Musk"));
+
+
+    }
+    public List<Person> getPersons() {
+        return this.persons;
+    }
+
+    public void addPerson(Person p) {
+        persons.add(new Person(p.getFirstName(), p.getLastName()));
+        
+    }
+}
